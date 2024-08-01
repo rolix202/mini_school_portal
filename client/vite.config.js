97d2a70f3c7dc9e531fbe6a16f8df8875fc2ab74
rolix202 <williamsroland147@gmail.com>
2024-08-01 23:28:08 +0100

    updated the vite.config file with render url

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api':{
-        target: 'http://localhost:4000/api', changeOrigin: true,
+        target: `${API_URL}/api`, changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
     }
